d9d725fee7473c2ae0baf477a9ba500789a177ea
caysem <cayse.mcree@gmail.com>
2018-04-13 14:06:32 -0400

    logic started on initialize 2.

---
+++ js/initialize.js
@@ -0,0 +1,20 @@
+var geocoder = new google.maps.Geocoder();
+console.log(geocoder);
+
+
+
+
+document.addEventListener('submit', function(e){
+    e.preventDefault(); 
+
+    let address; 
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') { var lat; var long;
+            lat = results[0].geometry.bounds.b.b;
+            long = results[0].geometry.bounds.f.b;
+            console.log(results);
+            console.log(lat);
+            console.log(long);
+        }
+    });
+});
